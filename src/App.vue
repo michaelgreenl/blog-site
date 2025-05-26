2c3ab904dfbe73639cbb459f5742f0a20c2948f8
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-05-26 00:42:38 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
