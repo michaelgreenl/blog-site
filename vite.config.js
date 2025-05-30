09c99a356935bb25d17b68ed8971fb8e9feeff45
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-05-30 16:34:42 -0400

    Added packages for rendering markdown

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    nodePolyfills({
+      global: true,
+      buffer: true,
+      process: true,
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
