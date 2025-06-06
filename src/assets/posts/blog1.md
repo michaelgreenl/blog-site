05dee559ca037db13ead7fa31e2f3983c374d5f5
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-06-06 17:52:33 -0400

    Added show more toggle logic for the post description

--- src/assets/posts/blog1.md
+++ src/assets/posts/blog1.md
@@ -2,7 +2,7 @@
 title: "My First Big Adventure"
 date: "2025-05-15"
 tags: ["travel", "personal"]
-description: "This is a blog post description"
+description: "This is a longer blog post description. Mollit irure adipisicing elit. Do labore ut minim aliqua officia in ea ad. Fugiat proident officia dolor mollit sit deserunt proident deserunt fugiat reprehenderit. Nulla qui cupidatat cillum fugiat proident non. Proident occaecat qui mollit duis velit in tempor aliquip pariatur duis laborum sit est do ex. Consectetur Lorem excepteur nostrud ipsum ea nostrud enim elit sit amet."
 ---
 
 # Blog 1
--- src/components/PostLinkCard.vue
+++ src/components/PostLinkCard.vue
@@ -1,18 +1,58 @@
 <script setup>
+import { ref, computed } from 'vue';
+
 const props = defineProps({
-  posts: Object,
+  post: Object,
 });
+
+const showMore = ref(false);
+const TRUNCATE_LENGTH = 100;
+
+const isLongDescription = computed(() => {
+  return props.post.description && props.post.description.length > TRUNCATE_LENGTH;
+});
+
+const displayDescription = computed(() => {
+  if (!isLongDescription.value || showMore.value) {
+    return props.post.description;
+  }
+  return props.post.description.substring(0, TRUNCATE_LENGTH) + '...';
+});
+
+function toggleShowMore() {
+  showMore.value = !showMore.value;
+}
 </script>
 
 <template>
-  <li v-for="post in props.posts" :key="post.slug">
-    <router-link :to="{ name: 'Post', params: { slug: post.slug } }">
-      <h2>{{ post.title }}</h2>
-    </router-link>
-    <!-- TODO: Add showMore logic for descriptions -->
-    <p v-if="post.description">{{ post.description }}</p>
-    <p v-if="post.date">{{ new Date(post.date).toLocaleDateString() }}</p>
-  </li>
+  <div>
+    <div class="post-link">
+      <router-link :to="{ name: 'Post', params: { slug: props.post.slug } }">
+        <h2>{{ props.post.title }}</h2>
+      </router-link>
+      <p v-if="props.post.date">{{ new Date(props.post.date).toLocaleDateString() }}</p>
+    </div>
+    <div>
+      Tags: <span v-for="tag in props.post.tags" :key="tag" class="tag">#{{ tag }}&nbsp;</span>
+    </div>
+    <p v-if="props.post.description">
+      {{ displayDescription }}
+      <span v-if="isLongDescription" @click="toggleShowMore" class="show-more">
+        {{ showMore ? 'Show Less' : 'Show More' }}
+      </span>
+    </p>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.post-link {
+  display: flex;
+  justify-content: space-between;
+}
+
+.show-more {
+  cursor: pointer;
+  font-weight: bold;
+  color: #aaa;
+}
+</style>
