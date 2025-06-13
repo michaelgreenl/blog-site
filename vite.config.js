15bd5e85931c30dc7d222ab38588a5ee99d6479e
Michael Green <57341710+michaelgreenl@users.noreply.github.com>
2025-06-12 21:08:11 -0400

    deploy prep

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/blog-site/',
   plugins: [
     vue(),
     vueDevTools(),
